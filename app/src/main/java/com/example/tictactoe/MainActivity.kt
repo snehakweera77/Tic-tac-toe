f385a5fb59bcb392f1ddd1084aa154fbdfb7dd4a
snehakweera77 <snehakweera77@gmail.com>
2021-09-04 22:33:51 +0530

    button click event

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+    fun btnClick(view: View) {
+        val btnSelected: Button = view as Button
+        var cellId = 0
+        when (btnSelected.id) {
+            R.id.button1 -> cellId = 1
+            R.id.button3 -> cellId = 2
+            R.id.button2 -> cellId = 3
+            R.id.button4 -> cellId = 4
+            R.id.button5 -> cellId = 5
+            R.id.button6 -> cellId = 6
+            R.id.button7 -> cellId = 7
+            R.id.button8 -> cellId = 8
+            R.id.button9 -> cellId = 9
+        }
     }
 }
