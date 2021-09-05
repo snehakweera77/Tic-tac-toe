85586eba353b9494d4eaf45a6713e0e720c3a91b
snehakweera77 <snehakweera77@gmail.com>
2021-09-05 22:14:17 +0530

    Winning logic

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var activePlayer = 1
+    var player1 = ArrayList<Int>()
+    var player2 = ArrayList<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,5 +30,69 @@ class MainActivity : AppCompatActivity() {
             R.id.button8 -> cellId = 8
             R.id.button9 -> cellId = 9
         }
+        playGame(cellId, btnSelected)
+    }
+    fun playGame(cellId: Int, btnSelected: Button) {
+        if (activePlayer == 1) {
+            btnSelected.text = "X"
+            btnSelected.setBackgroundResource(R.color.blue)
+            player1.add(cellId)
+        } else {
+            btnSelected.text = "O"
+            btnSelected.setBackgroundResource(R.color.darkGreen)
+            player2.add(cellId)
+        }
+        btnSelected.isEnabled = false
+        checkWinner()
+    }
+
+    private fun checkWinner() {
+        var winner = -1
+        //row 1
+        if (player1.contains(1) && player1.contains(2) && player1.contains(3))
+            winner = 1
+        if (player2.contains(1) && player2.contains(2) && player2.contains(3))
+            winner = 2
+        //row 2
+        if (player1.contains(4) && player1.contains(5) && player1.contains(6))
+            winner = 1
+        if (player2.contains(4) && player2.contains(5) && player2.contains(6))
+            winner = 2
+        //row 3
+        if (player1.contains(7) && player1.contains(8) && player1.contains(9))
+            winner = 1
+        if (player2.contains(7) && player2.contains(8) && player2.contains(9))
+            winner = 2
+        //col 1
+        if (player1.contains(1) && player1.contains(4) && player1.contains(7))
+            winner = 1
+        if (player2.contains(1) && player2.contains(4) && player2.contains(7))
+            winner = 2
+        //col 2
+        if (player1.contains(2) && player1.contains(5) && player1.contains(8))
+            winner = 1
+        if (player2.contains(2) && player2.contains(5) && player2.contains(8))
+            winner = 2
+        //col 3
+        if (player1.contains(3) && player1.contains(6) && player1.contains(9))
+            winner = 1
+        if (player2.contains(3) && player2.contains(6) && player2.contains(9))
+            winner = 2
+        //diag 1
+        if (player1.contains(1) && player1.contains(5) && player1.contains(9))
+            winner = 1
+        if (player2.contains(1) && player2.contains(5) && player2.contains(9))
+            winner = 2
+        //diag 2
+        if (player1.contains(3) && player1.contains(5) && player1.contains(7))
+            winner = 1
+        if (player2.contains(3) && player2.contains(5) && player2.contains(7))
+            winner = 2
+
+        if (winner == 1)
+            Toast.makeText(this, "1", Toast.LENGTH_SHORT).show()
+        else
+            Toast.makeText(this, "2", Toast.LENGTH_SHORT).show()
+
     }
 }
